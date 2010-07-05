5ef97fe0026e5d869fb9e949b48bd6cdf7f9492b
Taybin Rutkin <taybin@penguinsounds.org>
2010-07-05 14:36:45 -0400

    add --HEAD support to jack
    - necessary as the latest SVN is preferable to the last release.

--- Library/Formula/jack.rb
+++ Library/Formula/jack.rb
@@ -4,11 +4,24 @@ class Jack <Formula
   url 'http://jackaudio.org/downloads/jack-audio-connection-kit-0.118.0.tar.gz'
   homepage 'http://jackaudio.org'
   md5 'd58e29a55f285d54e75134cec8e02a10'
+  head 'http://subversion.jackaudio.org/jack/trunk/jack'
 
   depends_on 'pkg-config'
   depends_on 'celt' => :optional
 
+  def download_strategy
+    if ARGV.include? '--HEAD'
+      SubversionDownloadStrategy
+    else
+      CurlDownloadStrategy
+    end
+  end
+
   def install
+    if ARGV.include? '--HEAD'
+      ENV['ACLOCAL_FLAGS'] = "-I/usr/local/share/aclocal"
+      system "./autogen.sh"
+    end
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
